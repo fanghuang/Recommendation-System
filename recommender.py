cdb81b61cd4ee5d1534faa0ce21318a62f5b93f6
fanghuang <fanghuang1992@gmail.com>
2016-03-04 18:26:50 -0600

    recommend top x places base on place

--- recommender.py
+++ recommender.py
@@ -1,3 +1,4 @@
+u0 = ['a1']
 u1 = ['a1', 'a2', 'a3', 'a4']
 u2 = ['a1', 'a2', 'a3', 'a4']
 u3 = ['a5', 'a6', 'a7', 'a8']
@@ -21,5 +22,21 @@ class Helper(object):
 				count += 1
 		return count
 
-print Helper.cosine_similarity(u1,u4)
-print Helper.cosine_similarity(u1,u5)
+def recommend_top_5(place, user_history):
+	place_similarity = {}
+	for places in user_history:
+		similarity = Helper.cosine_similarity([place], places)
+		for i in places:
+			if place_similarity.has_key(i):
+				place_similarity[i] += similarity
+			else:
+				place_similarity[i] = similarity
+
+	if not place_similarity.has_key(place):
+		return "This is a brand new place that no one has ever visited here before"
+
+	place_similarity.pop(place)
+	sorted_place = sorted(place_similarity.items(), key=lambda x:x[1], reverse=True)
+	return sorted_place
+
+print recommend_top_5('a1', [u1,u4])
